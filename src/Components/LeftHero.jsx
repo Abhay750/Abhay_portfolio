a250eec58b49dc14c4ef7b85c8a6030ed5bf73d8
Abhay <abhay@Abhays-MacBook-Pro.local>
2026-02-19 16:29:41 +0530

    fix: update hero section links to use anchor tags

--- src/Components/LeftHero.jsx
+++ src/Components/LeftHero.jsx
@@ -10,8 +10,8 @@ const LeftHero = () => {
         A passionate web developer specializing in creating stunning and functional websites. With a keen eye for design and I bring your digital visions to life. Let's collaborate to build something amazing together!
       </p>
       <div className="buttons flex flex-wrap gap-4 mt-2">
-        <a href="/projects" className="px-6 py-3 flex items-center text-base md:text-xl justify-center text-black bg-green-500 transition-all duration-300 hover:shadow-[0_0_20px_rgba(34,197,94,0.6)] rounded-md">View Projects</a>
-        <a href="/about" className="px-6 py-3 flex items-center text-base md:text-xl justify-center text-white bg-transparent border border-white hover:bg-green-500/5 transition-all duration-300 hover:border-green-500/50 rounded-md">About Me</a>
+        <a href="#projects" className="px-6 py-3 flex items-center text-base md:text-xl justify-center text-black bg-green-500 transition-all duration-300 hover:shadow-[0_0_20px_rgba(34,197,94,0.6)] rounded-md">View Projects</a>
+        <a href="#about" className="px-6 py-3 flex items-center text-base md:text-xl justify-center text-white bg-transparent border border-white hover:bg-green-500/5 transition-all duration-300 hover:border-green-500/50 rounded-md">About Me</a>
       </div>
     </div>
   )
